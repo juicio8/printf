9f0b2ce85dd54f01febfec7fdf57316b8ef64551
juicio8 <you@example.com>
2022-08-08 05:00:24 -0700

    fixed

--- _printf.c
+++ _printf.c
@@ -4,33 +4,39 @@
 #include <unistd.h>
 /**
  * _putchars - writes characters.
- * @c - character
- * Return - character
+ * @c: character
+ * Return: character
  */
 int _putchars(char c)
 {
-	return (write (1, &c, 1));
+	return (write(1, &c, 1));
 }
 /**
  * print_string - prints string
- * @s - array of characters
+ * @s: array of characters
  */
 void print_string(char *s)
 {
 	int i = 0;
+
 	while (s[i] != '\0')
 	{
 		_putchars(s[i]);
 		i++;
 	}
 }
+/**
+ * _printf - printf function
+ * @format: format of arg
+ * Return: 0
+ */
 int _printf(const char *format, ...)
 {
 	const char *traverse;
 	unsigned int i;
 	char *s;
-
 	va_list arg;
+
 	va_start(arg, format);
 
 	for (traverse = format; *traverse != '\0'; traverse++)
